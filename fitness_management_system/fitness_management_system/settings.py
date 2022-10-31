30204bed30d3cc1c53f06b629705ba1943ce5136
zhouzhouce <zhouzhouce@gmail.com>
2022-10-31 00:43:24 -0400

    Created fitness_testdb using mysql

--- fitness_management_system/fitness_management_system/settings.py
+++ fitness_management_system/fitness_management_system/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'fitness_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fitness_testdb',
+        'USER': 'test',
+        'PASSWORD': 'secret_1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
--- fitness_management_system/login/models.py
+++ fitness_management_system/login/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+
+class UserProfile(models.Model):
+    BEGINNER = 'Beginner'
+    INTERMEDIATE = 'Intermediate'
+    DIFFICULT = 'Difficult'
+    EXPERT = 'Export'
+    CHOICES = (
+        (BEGINNER, BEGINNER),
+        (INTERMEDIATE, INTERMEDIATE),
+        (DIFFICULT, DIFFICULT),
+        (EXPERT, EXPERT)
+    )
+    name = models.CharField(max_length=100, unique=True)
+    date_of_birth = models.DateField()
+    height = models.TextField(blank=True, null=True)
+    weight = models.TextField(blank=True, null=True)
+    training_level = models.CharField(max_length=100, choices=CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+    exercise = models.TextField()
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return self.exercise
+
+
+class EventData(models.Model):
+    duration = models.IntegerField()
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
