c150403293527385bf082ae0afd7a484328a03d4
czhao051 <czhao051@uottawa.ca>
2022-10-30 20:58:39 -0400

    finish login static page

---
+++ fitness_management_system/login/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+
+def login(request):
+    pass
+    return render(request, 'login/login.html')
