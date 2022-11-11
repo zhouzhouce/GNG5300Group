58cd8a5e38a5852a866d747947e8bd2457641f18
zhouzhouce <zhouzhouce@gmail.com>
2022-11-11 12:39:34 -0500

    Add created_at field for event data and utils methods
    Add created_at field for model EventData to indicate the date when the event record creates.
    Add generate_event_data method in utils.py to create a new event or update an existing event.
    Add calculate_calories method to calculate total calories that the user burned today.
    Add get_event_history method to let users download their own personal event history.

--- fitness_management_system/login/models.py
+++ fitness_management_system/login/models.py
@@ -1,9 +1,41 @@
 from django.db import models
-from login import utils
 from django_mysql.models import EnumField
+import datetime
 
 
 # Create your models here.
+class LevelEnum(models.TextChoices):
+    ENTRY = 'ENTRY'
+    MEDIUM = 'MEDIUM'
+    HIGH = 'HIGH'
+
+
+class GenderEnum(models.TextChoices):
+    FEMALE = 'FEMALE'
+    MALE = 'MALE'
+    UNDEFINED = 'UNDEFINED'
+
+
+class AgeEnum(models.TextChoices):
+    AGE_RANGE_0_29 = 'AGE_RANGE_0_29'
+    AGE_RANGE_30_40 = 'AGE_RANGE_30_40'
+    AGE_RANGE_40_50 = 'AGE_RANGE_40_50'
+    AGE_ABOVE_50 = 'AGE_ABOVE_50'
+    UNDEFINED = 'UNDEFINED'
+
+
+class ExerciseTitleEnum(models.TextChoices):
+    HIIT = 'HITT'
+    YOGA = 'YOGA'
+    CARDIO = 'CARDIO'
+    BOOTY = 'BOOTY'
+    AB_WORKOUT = 'AB_WORKOUT'
+    WAIST = 'WAIST'
+    CIRCUIT = 'CIRCUIT'
+    STRENGTH = 'STRENGTH'
+    FAT_BURNING = 'FAT_BURNING'
+
+
 class User(models.Model):
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
@@ -15,17 +47,17 @@ class User(models.Model):
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, unique=True)
-    age = EnumField(choices=utils.AgeEnum.choices, default='UNDEFINED')
-    gender = EnumField(choices=utils.GenderEnum.choices, default='UNDEFINED')
-    level = EnumField(choices=utils.LevelEnum.choices, default='ENTRY')
+    age = EnumField(choices=AgeEnum.choices, default='UNDEFINED')
+    gender = EnumField(choices=GenderEnum.choices, default='UNDEFINED')
+    level = EnumField(choices=LevelEnum.choices, default='ENTRY')
 
     def __str__(self):
         return self.name
 
 
 class Exercise(models.Model):
-    exercise_title = EnumField(choices=utils.ExerciseTitleEnum.choices, default='HITT')
-    level = EnumField(choices=utils.LevelEnum.choices, default='ENTRY')
+    exercise_title = EnumField(choices=ExerciseTitleEnum.choices, default='HITT')
+    level = EnumField(choices=LevelEnum.choices, default='ENTRY')
     duration = models.FloatField(default=0)
     calories = models.IntegerField(default=0)
     link = models.URLField(max_length=200, default='https://www.youtube.com/')
@@ -38,10 +70,7 @@ class EventData(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     exercise_times = models.IntegerField()
+    created_at = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return 'Event_' + self.id
-
-
-
-
